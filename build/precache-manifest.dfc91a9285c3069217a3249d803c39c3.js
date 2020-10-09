ef38eb1190662c6fe9957f100f36ce066b478756
linyingcong <linyingcong@edusoho.com>
2020-10-09 16:14:31 +0800

    10.9 build test

---
+++ build/precache-manifest.dfc91a9285c3069217a3249d803c39c3.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f617ded0bcd31cca392ab509e2943b88",
+    "url": "/congb19-top/build/index.html"
+  },
+  {
+    "revision": "6367d79306a6361505b4",
+    "url": "/congb19-top/build/static/css/2.220ccb42.chunk.css"
+  },
+  {
+    "revision": "5a1888e900477f861c5d",
+    "url": "/congb19-top/build/static/css/4.e810b2df.chunk.css"
+  },
+  {
+    "revision": "b037199b45390a3618e4",
+    "url": "/congb19-top/build/static/css/5.1231a5ef.chunk.css"
+  },
+  {
+    "revision": "ae6df322469243b2b52f",
+    "url": "/congb19-top/build/static/css/6.1231a5ef.chunk.css"
+  },
+  {
+    "revision": "9ccbef4aaa51e198900b",
+    "url": "/congb19-top/build/static/css/main.16369bfc.chunk.css"
+  },
+  {
+    "revision": "6367d79306a6361505b4",
+    "url": "/congb19-top/build/static/js/2.2fea12dc.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/congb19-top/build/static/js/2.2fea12dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a3cb962d3b1d78b40d5",
+    "url": "/congb19-top/build/static/js/3.0e880c70.chunk.js"
+  },
+  {
+    "revision": "8e7fa176b006150306288bd092a696c0",
+    "url": "/congb19-top/build/static/js/3.0e880c70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a1888e900477f861c5d",
+    "url": "/congb19-top/build/static/js/4.d39313b8.chunk.js"
+  },
+  {
+    "revision": "b037199b45390a3618e4",
+    "url": "/congb19-top/build/static/js/5.f5a73d0a.chunk.js"
+  },
+  {
+    "revision": "ae6df322469243b2b52f",
+    "url": "/congb19-top/build/static/js/6.ca1d4b76.chunk.js"
+  },
+  {
+    "revision": "524b9f9eb0618c6f6c08",
+    "url": "/congb19-top/build/static/js/7.a386265b.chunk.js"
+  },
+  {
+    "revision": "c2e04c1eb2a8d0128cc7",
+    "url": "/congb19-top/build/static/js/8.495e06c3.chunk.js"
+  },
+  {
+    "revision": "9ccbef4aaa51e198900b",
+    "url": "/congb19-top/build/static/js/main.dc2769b1.chunk.js"
+  },
+  {
+    "revision": "7589ee7ae232dc583c07",
+    "url": "/congb19-top/build/static/js/runtime-main.d9376571.js"
+  },
+  {
+    "revision": "09719954d74dba48ebb8ade68b73ae86",
+    "url": "/congb19-top/build/static/media/banner1.09719954.jpg"
+  }
+]);
