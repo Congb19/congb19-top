de512e149ca021e3dda1d1c99b19f0cb5907e8c6
congb19 <347006087@qq.com>
2021-04-26 20:11:29 +0800

    feat: eva test

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,12 +3,14 @@ import { createRouter, createWebHistory } from 'vue-router';
 // import HelloWorld from '../components/HelloWorld.vue';
 
 import index from '../pages/index.vue';
+import game from '../pages/game.vue';
 const About = { template: '<div>About page</div>' };
 
 // routes
 const routes = [
 	{ path: '/', component: index },
 	{ path: '/about', component: About },
+	{ path: '/game', component: game },
 ];
 
 const router = createRouter({
