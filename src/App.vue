e4773e026d635b10775f523c80dc32e1a1d8c5f0
congb19 <347006087@qq.com>
2021-08-13 17:45:18 +0800

    feat: hstest

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav">
-    <router-link to="/"> Home</router-link>
-    <!-- <router-link to="/game"> Game</router-link> -->
+    <router-link to="/"> Home </router-link>
+    <router-link to="/game"> Game </router-link>
     <!-- <router-link to="/about">about </router-link> -->
   </div>
   <router-view></router-view>
