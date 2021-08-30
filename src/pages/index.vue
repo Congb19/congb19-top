a4af86cdfde1ccf8b6cc6e880cf3b29a8168d323
Congb19 <347006087@qq.com>
2021-08-30 14:41:00 +0800

    feat: 首页更改

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,11 +2,21 @@
   <img alt="logo" width="200" src="../assets/klee.jpeg" />
   <n-h1>你在烦恼什么呢</n-h1>
   <n-h2>这里是Congb19的小站！大家要天天开心</n-h2>
-  <n-h3>有烦恼请随时联系我：</n-h3>
-  <n-li><n-a href="mailto:linyc@congb19.com">linyc@congb19.com</n-a></n-li>
-  <n-li><n-a href="https://blog.congb19.com">Congb19的博客</n-a></n-li>
-
-  <n-p>这个网站不知道写点什么了，走过路过来点idea吧~~</n-p>
+  <n-h3>有烦恼🤯请随时联系我🍬：</n-h3>
+  <n-li>
+    TG 🛫 悄悄聊:
+    <n-a target="_blank" href="http://t.me/Congb19">@Congb19</n-a>
+  </n-li>
+  <n-li>
+    超私人的 📧 :
+    <n-a href="mailto:linyc@congb19.com">linyc@congb19.com</n-a>
+  </n-li>
+  <n-p>这个网站不知道写点什么了😭，走过路过来点💡吧~~</n-p>
+  <n-p>我的其他站</n-p>
+  <n-li>
+    写小作文的地方：
+    <n-a target="_blank" href="https://blog.congb19.com">Congb19的博客</n-a>
+  </n-li>
 </template>
 
 <script lang="ts">
