2793bd9bf2a4d43222061f0e48734ee22e66da71
Congb19 <347006087@qq.com>
2022-01-13 17:23:48 +0800

    feat: wall改版 1.13

--- src/components/wall-item.vue
+++ src/components/wall-item.vue
@@ -1,28 +1,10 @@
 <template>
-  <n-card :title="info.title" size="medium">
-    <template #cover>
-      <img :src="info.imgsrc" />
-    </template>
-    {{ info.content }}
+  <n-card title="asd" size="medium">
+    wall item
   </n-card>
 </template>
-<script lang="ts">
+<script setup lang="ts">
 import { ref, defineComponent, onMounted } from 'vue';
-export default defineComponent({
-  props: {
-    info: {
-      type: Object,
-      default: {
-        imgsrc: 'https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg',
-        title: 'default',
-        content: 'default',
-      },
-    },
-  },
-  setup: (props) => {
-    console.log('kbn-item', props.info);
-  },
-});
 </script>
 <style lang="scss" scoped>
 .n-card {
--- src/pages/wall.vue
+++ src/pages/wall.vue
@@ -1,38 +1,57 @@
 <template>
-  <n-collapse default-expanded-names="['1', '2']">
-    <n-collapse-item title="GAME!" name="1">
-      <wall-item v-for="item in gameList" :info="item"></wall-item>
-    </n-collapse-item>
-    <n-collapse-item title="OTHERS" name="2">
-      <div>ASD</div>
-    </n-collapse-item>
-  </n-collapse>
+  <div ref="self" class="cb-pool" v-on="dragEvent"><WallItem></WallItem></div>
 </template>
 
-<script lang="ts">
-import { ref, defineComponent, onMounted } from 'vue';
-import store from '../store';
+<script setup lang="ts">
+import { ref, reactive } from 'vue';
 import WallItem from '../components/wall-item.vue';
-export default defineComponent({
-  components: {
-    WallItem,
-  },
-  setup: () => {
-    let gameList = ref([
-      {
-        imgsrc: 'https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg',
-        title: '123title',
-        content: '123content',
-      },
-    ]);
-    const initData = async () => {};
-    onMounted(async () => {
-      console.log('wall');
-      await initData();
-    });
-    return { gameList, msg: store.state.message };
-  },
+
+type Position = {
+  x: Number;
+  y: Number;
+};
+
+const self = ref(null)
+console.log(self)
+// const selfPos = {x:self.value?.offsetLeft, y:self.value?.offsetTop};
+// console.log(selfPos)
+
+let pressed = ref(false);
+let dragStartPoint: Position = reactive({
+  x: 0,
+  y: 0,
+});
+
+const moving = (pos: Position) => {
+  // console.log(pos.x, pos.y);
+};
+
+const dragStart = (pos: Position) => {
+  pressed.value = true;
+  dragStartPoint = { x: pos.x, y: pos.y };
+  console.log('dragStart', dragStartPoint);
+};
+const dragging = (pos: Position) => {
+  // console.log(pos.x, pos.y);
+};
+const dragStop = (pos: Position) => {
+  pressed.value = false;
+  console.log('dragStop');
+};
+const dragEvent = ref({
+  mousedown: dragStart,
+  mousemove: pressed.value ? dragging : moving,
+  mouseup: dragStop,
 });
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.cb-pool {
+  border: 0;
+  margin: 0;
+  padding: 0;
+  background-color: aqua;
+  width: 3000px;
+  height: 2000px;
+}
+</style>
